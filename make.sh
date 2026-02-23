e342815658bd0c96e15d82308e96aa629a3c8495
Matthieu NAJM <matthieu.najm@pasteur.fr>
2026-02-23 15:48:33 +0100

    fix: correct DECIDER files

--- make.sh
+++ make.sh
@@ -77,13 +77,11 @@ cmd="uv run python3 ${py_args} $script_dir/weave.py \
     --copy-number-amplifications-local      $data_dir/DECIDER/$data_version/cnas_v2.9_local.csv \
     --copy-number-amplifications-external   $data_dir/DECIDER/$data_version/cnas_v2.9_external.csv  \
     ${weave_args}" # \
-    # --clinical                              $data_dir/DECIDER/$data_version/clinical_export.csv \
-    # --short-mutations-local                 $data_dir/DECIDER/$data_version/snv_local.csv \
-    # --short-mutations-external              $data_dir/DECIDER/$data_version/snv_external.csv  \
-    # --copy-number-amplifications-local      $data_dir/DECIDER/$data_version/cna_local.csv \
-    # --copy-number-amplifications-external   $data_dir/DECIDER/$data_version/cna_external.csv  \
-    # --clinical                   $data_dir/DECIDER/$data_version/clinical_export.csv \
-    # --copy_number_alterations    $data_dir/DECIDER/$data_version/cna_external.csv \
+    # --clinical                              $data_dir/DECIDER/$data_version/clinical_export_2024-11-13.csv \
+    # --short-mutations-local                 $data_dir/DECIDER/$data_version/short_mutations_v4.10_local.csv \
+    # --short-mutations-external              $data_dir/DECIDER/$data_version/short_mutations_v4.10_external.csv  \
+    # --copy-number-amplifications-local      $data_dir/DECIDER/$data_version/cnas_v2.9_local.csv \
+    # --copy-number-amplifications-external   $data_dir/DECIDER/$data_version/cnas_v2.9_external.csv  \
     # --gene_ontology_genes        $data_dir/DECIDER/$data_version/OncoKB_gene_symbols.conf \
     # --oncokb                     $data_dir/DECIDER/$data_version/treatments.csv \
     # --gene_ontology              $data_dir/GO/goa_human.gaf.gz \
